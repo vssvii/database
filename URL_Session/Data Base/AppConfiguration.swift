998c20386976ffb05d89139125d18b77d2e8301f
Ibragim Assaibuldayev <Ibragimktl95@gmail.com>
2022-07-09 21:21:55 +0600

    "1st version"

---
+++ URL_Session/Data Base/AppConfiguration.swift
@@ -0,0 +1,17 @@
+//
+//  AppConfiguration.swift
+//  Navigation_2
+//
+//  Created by Ibragim Assaibuldayev on 05.07.2022.
+//
+
+import Foundation
+import UIKit
+
+enum AppConfiguration {
+    
+    case people (urlString: String)
+    case starships (urlString: String)
+    case planets (urlString: String)
+
+}
