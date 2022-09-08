5ce9848d35cce484dff90bdab8f5fcdcc15b1f2e
Ibragim Assaibuldayev <Ibragimktl95@gmail.com>
2022-09-08 17:24:14 +0600

    "fixed creation of files and folders"

---
+++ Bundle_sandbox_filemanager/Models/Content.swift
@@ -0,0 +1,26 @@
+//
+//  Content.swift
+//  FileManager
+//
+//  Created by Ibragim Assaibuldayev on 01.09.2022.
+//
+
+
+import UIKit
+
+struct Content {
+    
+    enum ContentType {
+        case folder(url: URL?)
+        case file(url: URL?)
+        var url: URL? {
+            switch self {
+            case .folder(let url), .file(let url):
+                return url }
+        }
+    }
+    
+    let type: ContentType
+    var name: String
+}
+ 
